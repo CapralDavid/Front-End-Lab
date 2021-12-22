5d609a249e209c5178941f5e49427a6034acfe02
CapralDavid <ssdrevcukss@gmail.com>
2021-12-22 14:32:58 +0100

    ss

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import {  AddShoppingCart } from '@material-ui/icons';
-import Product from './components/Product/Product';
+import Product from './components/Product/Product.jsx';
 import useStyles from './styles'
 
 const Product = ({ product }) => {
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
-import Product from './Product/Product';
+import Product from './Product/Product.jsx';
 
 
 const products = [

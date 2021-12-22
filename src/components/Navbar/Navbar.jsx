8cac86331e24043ef46c10222d86d8a81050fc4e
CapralDavid <ssdrevcukss@gmail.com>
2021-12-22 18:00:47 +0100

    Nav bar fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
-import { Link, useLocation } from 'react-router-dom';
 import logo from '../../assets/logo.ico'
 import useStyles from './styles'
 
@@ -13,13 +12,13 @@ const Navbar = () => {
          <AppBar postion="fixed" className={classes.AppBar} color="inherit "    > 
          <Toolbar>
              <Typography variant="h6" className={classes.tittle} color="inherit" >
-                 <img src={logo} alt='Commerce.js' height="25px" className={classes.image}  />
+                 <img src={logo} alt='Richard Sells' height="25px" className={classes.image}  />
                  Richard Sells
              </Typography>
              <div className={classes.grow} />
              <div className='classes.button'>
                  <IconButton aria-label="Show the cart items" color="inherit">
-                     <Badge badgeContent={2} colot="secondary"> 
+                     <Badge badgeContent={6} color="secondary"> 
                      <ShoppingCart />
                      </Badge>
 

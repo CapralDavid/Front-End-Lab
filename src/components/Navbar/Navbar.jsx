524044bddea20e591946fbc852c974091668d27c
CapralDavid <ssdrevcukss@gmail.com>
2021-12-22 14:48:43 +0100

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,11 @@ import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '
 import { ShoppingCart } from '@material-ui/icons';
 import { Link, useLocation } from 'react-router-dom';
 import logo from '../../assets/logo.ico'
+import useStyles from './styles'
 
 
 const Navbar = () => {
+    const classes = useStyles()
     return (
         <>
          <AppBar postion="fixed" className={classes.AppBar} color="inherit "    > 
@@ -14,6 +16,15 @@ const Navbar = () => {
                  <img src={logo} alt='Commerce.js' height="25px" className={classes.image}  />
                  Richard Sells
              </Typography>
+             <div className={classes.grow} />
+             <div className='classes.button'>
+                 <IconButton aria-label="Show the cart items" color="inherit">
+                     <Badge badgeContent={2} colot="secondary"> 
+                     <ShoppingCart />
+                     </Badge>
+
+                 </IconButton>
+             </div>
          </Toolbar>
          </AppBar>
         </>
